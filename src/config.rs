f0dff60d74d6746095ca6806085bf16602a287eb
Harsh Shandilya <me@msfjarvis.dev>
2020-06-06 03:42:15 +0530

    config: add UUID validation
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- src/config.rs
+++ src/config.rs
@@ -1,9 +1,15 @@
+use uuid::Uuid;
+
 pub struct HealthcheckConfig {
     pub uuid: String,
 }
 
 pub fn create_config(uuid: &str) -> HealthcheckConfig {
-    HealthcheckConfig {
-        uuid: uuid.to_string(),
+    if let Ok(_) = Uuid::parse_str(uuid) {
+        HealthcheckConfig {
+            uuid: uuid.to_string(),
+        }
+    } else {
+        panic!("Invalid UUID: {}", uuid)
     }
 }

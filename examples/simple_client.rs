66017498f9fdefa2a2a624e8215a07e17f48f61b
Harsh Shandilya <me@msfjarvis.dev>
2020-06-07 02:04:28 +0530

    examples/simple_client: take UUID from args
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- examples/simple_client.rs
+++ examples/simple_client.rs
@@ -3,7 +3,8 @@ extern crate healthchecks;
 use healthchecks::create_config;
 
 fn main() {
-    let config = create_config("my-uuid-that-is-definitely-not-real");
+    let uuid = std::env::args().nth(1).expect("Providing a UUID as first parameter is mandatory");
+    let config = create_config(&uuid);
     config.start_timer();
     std::thread::sleep(std::time::Duration::from_millis(10_000));
     config.report_success();

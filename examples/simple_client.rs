bd80a71284c32a34944b708201ed0346a45ff0e3
Harsh Shandilya <me@msfjarvis.dev>
2020-06-06 23:37:09 +0530

    examples/simple_client: add timer sample
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- examples/simple_client.rs
+++ examples/simple_client.rs
@@ -4,6 +4,7 @@ use healthchecks::create_config;
 
 fn main() {
     let config = create_config("my-uuid-that-is-definitely-not-real");
+    config.start_timer();
+    std::thread::sleep(std::time::Duration::from_millis(10_000));
     config.report_success();
-    config.report_failure();
 }

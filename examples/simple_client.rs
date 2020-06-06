d6ae4bb72fd082ed2c15641c6c98d815f982550b
Harsh Shandilya <me@msfjarvis.dev>
2020-06-07 02:12:49 +0530

    src: allow passing custom user agent
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- examples/simple_client.rs
+++ examples/simple_client.rs
@@ -4,7 +4,7 @@ use healthchecks::create_config;
 
 fn main() {
     let uuid = std::env::args().nth(1).expect("Providing a UUID as first parameter is mandatory");
-    let config = create_config(&uuid);
+    let config = create_config(uuid);
     config.start_timer();
     std::thread::sleep(std::time::Duration::from_millis(10_000));
     config.report_success();
--- src/lib.rs
+++ src/lib.rs
@@ -12,15 +12,28 @@ fn get_user_agent() -> String {
 /// report status to healthchecks.io
 pub struct HealthcheckConfig {
     pub(crate) uuid: String,
+    pub(crate) user_agent: String,
 }
 
 /// Create an instance of [HealthcheckConfig](struct.HealthcheckConfig.html) from a str uuid.
 /// The method runs basic UUID validation and will panic if there's a failure parsing the provided
 /// uuid.
-pub fn create_config(uuid: &str) -> HealthcheckConfig {
-    if let Ok(_) = Uuid::parse_str(uuid) {
+pub fn create_config(uuid: String) -> HealthcheckConfig {
+    if let Ok(_) = Uuid::parse_str(&uuid) {
         HealthcheckConfig {
-            uuid: uuid.to_string(),
+            uuid: uuid,
+            user_agent: get_user_agent(),
+        }
+    } else {
+        panic!("Invalid UUID: {}", uuid)
+    }
+}
+
+pub fn create_config_with_user_agent(uuid: String, user_agent: String) -> HealthcheckConfig {
+    if let Ok(_) = Uuid::parse_str(&uuid) {
+        HealthcheckConfig {
+            uuid: uuid,
+            user_agent: user_agent,
         }
     } else {
         panic!("Invalid UUID: {}", uuid)
@@ -31,7 +44,7 @@ impl HealthcheckConfig {
     /// Report success to healthchecks.io. Returns a boolean indicating whether the request succeeded.
     pub fn report_success(&self) -> bool {
         let res = get(&format!("{}/{}", HEALTHCHECK_PING_URL, self.uuid))
-            .set("User-Agent", &get_user_agent())
+            .set("User-Agent", &self.user_agent)
             .call();
         res.status() == 200
     }
@@ -39,7 +52,7 @@ impl HealthcheckConfig {
     /// Report failure to healthchecks.io. Returns a boolean indicating whether the request succeeded.
     pub fn report_failure(&self) -> bool {
         let res = get(&format!("{}/{}/fail", HEALTHCHECK_PING_URL, self.uuid))
-            .set("User-Agent", &get_user_agent())
+            .set("User-Agent", &self.user_agent)
             .call();
         res.status() == 200
     }
@@ -47,7 +60,7 @@ impl HealthcheckConfig {
     /// Start a timer on healthchecks.io, to measure script run times. Official documentation for it is available [here](https://healthchecks.io/docs/measuring_script_run_time/).
     pub fn start_timer(&self) -> bool {
         let res = get(&format!("{}/{}/start", HEALTHCHECK_PING_URL, self.uuid))
-            .set("User-Agent", &get_user_agent())
+            .set("User-Agent", &self.user_agent)
             .call();
         res.status() == 200
     }

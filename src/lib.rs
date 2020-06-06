f8f3ae4249a0738ad562e496182d28ee01c386c6
Harsh Shandilya <me@msfjarvis.dev>
2020-06-07 02:02:36 +0530

    src: make user agent dynamic
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 use ureq::get;
 use uuid::Uuid;
 
-const USER_AGENT: &'static str = "healthchecks-rs/0.1.0";
 const HEALTHCHECK_PING_URL: &'static str = "https://hc-ping.com";
+
+fn get_user_agent() -> String {
+    format!("healthchecks-rs/{}", env!("CARGO_PKG_VERSION"))
+}
 
 /// Struct that encapsulates the UUID that uniquely identifies your
 /// healthchecks.io endpoint. Instances of this exposes methods to
@@ -28,7 +31,7 @@ impl HealthcheckConfig {
     /// Report success to healthchecks.io. Returns a boolean indicating whether the request succeeded.
     pub fn report_success(&self) -> bool {
         let res = get(&format!("{}/{}", HEALTHCHECK_PING_URL, self.uuid))
-            .set("User-Agent", USER_AGENT)
+            .set("User-Agent", &get_user_agent())
             .call();
         res.status() == 200
     }
@@ -36,7 +39,7 @@ impl HealthcheckConfig {
     /// Report failure to healthchecks.io. Returns a boolean indicating whether the request succeeded.
     pub fn report_failure(&self) -> bool {
         let res = get(&format!("{}/{}/fail", HEALTHCHECK_PING_URL, self.uuid))
-            .set("User-Agent", USER_AGENT)
+            .set("User-Agent", &get_user_agent())
             .call();
         res.status() == 200
     }
@@ -44,7 +47,7 @@ impl HealthcheckConfig {
     /// Start a timer on healthchecks.io, to measure script run times. Official documentation for it is available [here](https://healthchecks.io/docs/measuring_script_run_time/).
     pub fn start_timer(&self) -> bool {
         let res = get(&format!("{}/{}/start", HEALTHCHECK_PING_URL, self.uuid))
-            .set("User-Agent", USER_AGENT)
+            .set("User-Agent", &get_user_agent())
             .call();
         res.status() == 200
     }

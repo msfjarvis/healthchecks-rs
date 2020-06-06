c96125b77b52b21df68f4e1eac555a25db2ec2e8
Harsh Shandilya <me@msfjarvis.dev>
2020-06-07 02:13:03 +0530

    examples: add sample client with custom user agent
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

---
+++ examples/simple_client_custom_user_agent.rs
@@ -0,0 +1,11 @@
+extern crate healthchecks;
+
+use healthchecks::create_config_with_user_agent;
+
+fn main() {
+    let uuid = std::env::args().nth(1).expect("Providing a UUID as first parameter is mandatory");
+    let config = create_config_with_user_agent(uuid, String::from("healthchecks-rs/simple_client"));
+    config.start_timer();
+    std::thread::sleep(std::time::Duration::from_millis(10_000));
+    config.report_success();
+}

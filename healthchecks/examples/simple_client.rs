9a208bc640ec734f4b70811525ce78c136eb0206
Harsh Shandilya <me@msfjarvis.dev>
2020-06-09 13:36:35 +0530

    examples: reformat
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- healthchecks/examples/simple_client.rs
+++ healthchecks/examples/simple_client.rs
@@ -3,7 +3,9 @@ extern crate healthchecks;
 use healthchecks::create_config;
 
 fn main() {
-    let uuid = std::env::args().nth(1).expect("Providing a UUID as first parameter is mandatory");
+    let uuid = std::env::args()
+        .nth(1)
+        .expect("Providing a UUID as first parameter is mandatory");
     let config = create_config(uuid);
     config.start_timer();
     std::thread::sleep(std::time::Duration::from_millis(10_000));
--- healthchecks/examples/simple_client_custom_user_agent.rs
+++ healthchecks/examples/simple_client_custom_user_agent.rs
@@ -3,7 +3,9 @@ extern crate healthchecks;
 use healthchecks::create_config_with_user_agent;
 
 fn main() {
-    let uuid = std::env::args().nth(1).expect("Providing a UUID as first parameter is mandatory");
+    let uuid = std::env::args()
+        .nth(1)
+        .expect("Providing a UUID as first parameter is mandatory");
     let config = create_config_with_user_agent(uuid, String::from("healthchecks-rs/simple_client"));
     config.start_timer();
     std::thread::sleep(std::time::Duration::from_millis(10_000));

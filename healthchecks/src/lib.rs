32384f0e7b46b0b9563ceda88bc16a9f3dc5b191
Harsh Shandilya <me@msfjarvis.dev>
2020-07-15 03:04:46 +0530

    healthchecks: document modules
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- healthchecks/src/lib.rs
+++ healthchecks/src/lib.rs
@@ -1,4 +1,7 @@
+/// Functions for interacting with the Healthchecks management API.
 pub mod manage;
+/// API response models for data returned from the various Healthchecks APIs.
 pub mod model;
+/// Functions for interacting with the Healthchecks pinging API.
 pub mod ping;
 pub(crate) mod util;

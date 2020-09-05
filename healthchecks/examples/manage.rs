d6df0984899445a1e1a3d162dc14bcc5ff026a29
Harsh Shandilya <me@msfjarvis.dev>
2020-09-05 08:47:09 +0530

    healthchecks: update example to include creation of new check
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- healthchecks/examples/manage.rs
+++ healthchecks/examples/manage.rs
@@ -1,4 +1,5 @@
 use healthchecks::manage::create_config;
+use healthchecks::model::NewCheck;
 
 fn main() -> anyhow::Result<()> {
     let api_key = std::env::args()
@@ -8,5 +9,18 @@ fn main() -> anyhow::Result<()> {
     for check in config.get_checks()? {
         println!("{:?}", check);
     }
+    let new_check  = NewCheck {
+        name: None,
+        tags: None,
+        channels: None,
+        desc: None,
+        timeout: None,
+        grace: None,
+        schedule: None,
+        tz: None,
+        unique: None,
+        manual_resume: None,
+    };
+    println!("{:?}", config.create_check(new_check)?);
     Ok(())
 }

msfjarvis/healthchecks-rs

commit acd8013150f437141f401b64d289d74f1733450c

monitor: bring back features dropped by clap v3 upgrade
Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

monitor/src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Context;
-use clap::{crate_version, Clap};
+use clap::{crate_authors, crate_version, AppSettings, Clap};
 use healthchecks::ping::get_config;
 use std::env::var;
 use std::process::Command;
@@ -12,7 +12,13 @@ struct Settings {
 
 /// monitor runs the given command and reports execution result to https://healthchecks.io
 #[derive(Clap)]
-#[clap(version = crate_version!(), author = "Harsh Shandilya <me@msfjarvis.dev>")]
+#[clap(
+    version = crate_version!(),
+    author = crate_authors!(),
+    setting = AppSettings::ColoredHelp,
+    setting = AppSettings::DeriveDisplayOrder,
+    setting = AppSettings::SubcommandRequiredElseHelp,
+)]
 struct Opts {
     /// command to execute and monitor
     #[clap(short = 'X', long = "exec")]

commit 446feb0b9960c93afaab96daa6dee07159c05e4f

monitor: remove incorrect SubcommandRequiredElseHelp setting
Fixes: acd8013150f4 ("monitor: bring back features dropped by clap v3 upgrade")
Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

monitor/src/main.rs
@@ -17,7 +17,6 @@ struct Settings {
     author = crate_authors!(),
     setting = AppSettings::ColoredHelp,
     setting = AppSettings::DeriveDisplayOrder,
-    setting = AppSettings::SubcommandRequiredElseHelp,
 )]
 struct Opts {
     /// command to execute and monitor
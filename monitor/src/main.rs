430366c5ff691c0472df437e57753dfd56b2fb2e
Harsh Shandilya <me@msfjarvis.dev>
2020-11-18 12:06:27 +0530

    monitor: switch to subprocess for executing commands
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- monitor/src/main.rs
+++ monitor/src/main.rs
@@ -2,7 +2,7 @@ use anyhow::{anyhow, Context};
 use clap::{crate_authors, crate_version, AppSettings, Clap};
 use healthchecks::ping::get_config;
 use std::env::var;
-use std::process::Command;
+use subprocess::Exec;
 
 const HEALTHCHECKS_CHECK_ID_VAR: &str = "HEALTHCHECKS_CHECK_ID";
 
@@ -43,21 +43,6 @@ fn main() -> anyhow::Result<()> {
         },
         ua,
     };
-    let commands: Vec<Vec<String>> = if opts.command.len() == 1 {
-        opts.command
-            .get(0)
-            .expect("This definitely has one command")
-            .split(';')
-            .map(|c| {
-                c.split(' ')
-                    .filter(|x| !x.is_empty())
-                    .map(|x| x.to_string())
-                    .collect()
-            })
-            .collect()
-    } else {
-        vec![opts.command]
-    };
     let mut config = get_config(&settings.check_id)?;
     if let Some(user_agent) = settings.ua {
         config = config.set_user_agent(&user_agent)
@@ -65,27 +50,12 @@ fn main() -> anyhow::Result<()> {
     if opts.timer {
         config.start_timer();
     }
-    for cmds in commands {
-        let mut command = Command::new(&cmds.get(0).expect("Should have at least one command"));
-        for cmd in cmds.iter().skip(1) {
-            command.arg(cmd);
-        }
-        match command
-            .status()
-            .context(format!("Failed on command: {:?}", cmds.join(" ")))?
-            .code()
-        {
-            Some(code) => {
-                if code != 0 {
-                    config.report_failure();
-                }
-            }
-            None => {
-                eprintln!("Interrupted!");
-                config.report_failure();
-            }
-        };
+    let cmd = opts.command.join(" ");
+    let exit_status = Exec::shell(&cmd).join().context(format!("Failed to execute {}", cmd))?;
+    if exit_status.success() {
+        config.report_success();
+    } else {
+        config.report_failure();
     }
-    config.report_success();
     Ok(())
 }

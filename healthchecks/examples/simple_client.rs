8430fb017461d9dd048d837b4047f9207a5cafd1
Harsh Shandilya <me@msfjarvis.dev>
2021-12-22 17:00:23 +0530

    healthchecks: fix lint warnings in examples

--- healthchecks/examples/simple_client.rs
+++ healthchecks/examples/simple_client.rs
@@ -1,14 +1,18 @@
-use healthchecks::errors::HealthchecksConfigError;
 use healthchecks::ping::get_client;
+use std::ops::Not;
 use std::result::Result;
 
-fn main() -> Result<(), HealthchecksConfigError> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let uuid = std::env::args()
         .nth(1)
         .expect("Providing a UUID as first parameter is mandatory");
     let config = get_client(&uuid)?;
-    config.start_timer();
+    if config.start_timer().not() {
+        panic!("Failed to start timer");
+    };
     std::thread::sleep(std::time::Duration::from_millis(10_000));
-    config.report_success();
+    if config.report_success().not() {
+        panic!("Failed to report success");
+    };
     Ok(())
 }

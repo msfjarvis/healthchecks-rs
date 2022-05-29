f052c3b017e2d912e655dedd52c532f6196e41c7
Harsh Shandilya <me@msfjarvis.dev>
2022-05-29 09:57:43 +0530

    feat: sync Nix config with dotfiles

--- shell.nix
+++ shell.nix
@@ -2,7 +2,8 @@ with import <nixpkgs> { overlays = [ (import <rust-overlay>) ]; };
 mkShell {
   buildInputs = [
     (rust-bin.stable.latest.default.override {
-      extensions = [ "rust-src" "rustfmt-preview" ];
+      extensions =
+        [ "rust-src" "rustc-dev" "rustfmt-preview" "llvm-tools-preview" ];
       targets =
         pkgs.lib.optionals pkgs.stdenv.isDarwin [ "aarch64-apple-darwin" ]
         ++ pkgs.lib.optionals pkgs.stdenv.isLinux

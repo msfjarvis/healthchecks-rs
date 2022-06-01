d40b37e1828cdb869a55ff43049122d87f1abbfd
Harsh Shandilya <me@msfjarvis.dev>
2022-06-01 11:09:17 +0530

    healthchecks: fix weird assert

--- healthchecks/examples/simple_client.rs
+++ healthchecks/examples/simple_client.rs
@@ -1,5 +1,4 @@
 use healthchecks::ping::get_client;
-use std::ops::Not;
 use std::result::Result;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -7,8 +6,8 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .nth(1)
         .expect("Providing a UUID as first parameter is mandatory");
     let config = get_client(&uuid)?;
-    assert!(!config.start_timer().not(), "Failed to start timer");
+    assert!(config.start_timer(), "Failed to start timer");
     std::thread::sleep(std::time::Duration::from_millis(10_000));
-    assert!(!config.report_success().not(), "Failed to report success");
+    assert!(config.report_success(), "Failed to report success");
     Ok(())
 }

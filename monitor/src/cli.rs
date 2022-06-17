f262808412cb31303cafd9e828ca6600f132ec29
Harsh Shandilya <me@msfjarvis.dev>
2022-06-17 13:44:09 +0530

    fix(deps): upgrade clap

--- monitor/src/cli.rs
+++ monitor/src/cli.rs
@@ -8,16 +8,22 @@ const FAKE_EMPTY_STRING: &str = "\"\"";
 #[clap(global_setting(AppSettings::DeriveDisplayOrder))]
 pub(crate) struct Opts {
     /// command to execute and monitor
-    #[clap(short = 'X', long = "exec")]
+    #[clap(short = 'X', long = "exec", action)]
     pub(crate) command: Vec<String>,
     /// starts a timer before running the command
-    #[clap(short = 't', long = "timer")]
+    #[clap(short = 't', long = "timer", action)]
     pub(crate) timer: bool,
     /// saves the execution logs with the failure ping to allow debugging on healthchecks.io
-    #[clap(short = 'l', long = "logs")]
+    #[clap(short = 'l', long = "logs", action)]
     pub(crate) save_logs: bool,
     /// number of times to retry the command before logging a failure
-    #[clap(short = 'r', long = "retries", default_value = "1", required = false)]
+    #[clap(
+        short = 'r',
+        long = "retries",
+        default_value = "1",
+        required = false,
+        action
+    )]
     pub(crate) retry_count: u8,
     /// user agent to be logged at healthchecks.io
     #[clap(
@@ -25,6 +31,7 @@ pub(crate) struct Opts {
         long = "user-agent",
         required = false,
         default_value = FAKE_EMPTY_STRING,
+        action,
     )]
     pub(crate) user_agent: String,
 }

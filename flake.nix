d5738d584e53179cd1356505d3a1e5cd56b93f0e
Harsh Shandilya <me@msfjarvis.dev>
2024-03-29 12:21:43 +0530

    fix: upgrade to Rust 1.77.0

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
 
       rustStable = (import fenix {inherit pkgs;}).fromToolchainFile {
         file = ./rust-toolchain.toml;
-        sha256 = "sha256-e4mlaJehWBymYxJGgnbuCObVlqMlQSilZ8FljG9zPHY=";
+        sha256 = "sha256-+syqAd2kX8KVa8/U2gz3blIQTTsYYt3U63xBWaGOSc8=";
       };
       rustMsrv = (fenix.packages.${system}.fromManifestFile rust-msrv).minimalToolchain;
 

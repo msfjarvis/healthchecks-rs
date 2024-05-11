29ca787fa1a7593630ec26b704479e17684d015a
Harsh Shandilya <me@msfjarvis.dev>
2024-05-11 11:56:45 +0530

    nix: use my nixpkgs fork

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "healthchecks-rs monorepo";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:msfjarvis/nixpkgs/nixpkgs-unstable";
 
   inputs.systems.url = "github:msfjarvis/flake-systems";
 

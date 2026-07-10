05a2de22c969de8ef2d1a7d0789de7009accee10
Harsh Shandilya <me@msfjarvis.dev>
2026-07-10 16:22:24 +0530

    fix(rust): update to v1.97.0

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
 
         rustStable = (import fenix { inherit pkgs; }).fromToolchainFile {
           file = ./rust-toolchain.toml;
-          sha256 = "sha256-h+t2xTBz5yt2YIO+1VMIIGlCU7gyp2LYOFvaV1nwOXU=";
+          sha256 = "sha256-OATSZm98Es5kIFuqaba+UvkQtFsVgJEBMmS+t6od5/U=";
         };
 
         craneLib = (crane.mkLib pkgs).overrideToolchain rustStable;

c8418f2b5629fc4ad8e248e3892bfd89d302e77a
Harsh Shandilya <me@msfjarvis.dev>
2026-09-22 20:23:26 +0530

    fix: move up nixpkgs pin

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "healthchecks-rs monorepo";
 
-  inputs.nixpkgs.url = "https://nixexprs.msfjarvis.dev/nixexprs.tar.zst";
+  inputs.nixpkgs.url = "https://nix-channels.msfjarvis.dev/nixpkgs/latest.tar.zst";
 
   inputs.systems.url = "github:msfjarvis/flake-systems";
 
